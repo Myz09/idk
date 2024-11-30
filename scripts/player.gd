b6073d60baf56eaff2e0e0608ff5a96ad5d1c100
Myz09 <160198749+Myz09@users.noreply.github.com>
2024-11-30 16:06:14 -0500

    11/30/2024 | 4:05
    changed sprites
    working on bullet
    added sounds

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 70.0
+const JUMP_VELOCITY = -250.0
+var double = true
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -13,9 +14,13 @@ func _physics_process(delta: float) -> void:
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or double):
+		if not is_on_floor():
+			double = false
 		velocity.y = JUMP_VELOCITY
-
+		$AudioStreamPlayer2D.play()
+	if is_on_floor():
+		double = true
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	#input direction: -1, 0, 1
@@ -23,9 +28,9 @@ func _physics_process(delta: float) -> void:
 	
 	#flip sprite
 	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
 		animated_sprite.flip_h = true
+	elif direction < 0:
+		animated_sprite.flip_h = false
 		
 	#play animation
 	if is_on_floor():
--- scripts/slim.gd
+++ scripts/slim.gd
@@ -6,14 +6,16 @@ var direction = 1
 @onready var raycastr: RayCast2D = $raycastr
 @onready var raycastl: RayCast2D = $raycastl
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+func _ready() -> void:
+	$AudioStreamPlayer2D.play()	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if raycastr.is_colliding():
 		direction = -1
-		animated_sprite.flip_h = true
+		animated_sprite.flip_h = false
 	if raycastl.is_colliding():
 		direction = 1
-		animated_sprite.flip_h = false
+		animated_sprite.flip_h = true
 	
 	position.x += direction * SPEED * delta
